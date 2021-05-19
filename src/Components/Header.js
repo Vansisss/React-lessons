1ead0f556bfbb53ecd122fac3b3a7a3d3f918333
Vansisss <ponom-vadim@ya.ru>
2021-05-19 19:55:13 +0300

    second commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Header.css'
 const Header =()=>{
 return (<div className='Header'>
        <img src='https://img-premium.flaticon.com/png/512/564/564419.png?token=exp=1621366057~hmac=aec4460dd087b64bfa60267bd6b56921'></img>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Nav.module.css'
     const Nav=()=>{
-        return(<div className='NavBar'>
-    <div>Profile</div>  
-    <div>Message</div>  
-    <div>News</div>
+        return(<div className={classes.NavBar}>
+    <div className={classes.item}>Profile</div>  
+    <div className={classes.item}>Message</div>  
+    <div className={classes.item}>News</div>
     <div>Friends</div>    
 </div>)
     }
